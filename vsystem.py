82345039b58bd07b62ccf9ad1c9e039bc376af99
LJ Wobker <ljwobker@pobox.com>
2023-03-08 09:46:00 -0500

    progress

--- vsystem.py
+++ vsystem.py
@@ -10,6 +10,9 @@ import datetime
 import batt_influx
 from dotenv import load_dotenv
 import os
+import argparse
+import sys
+
 
 
 
@@ -76,7 +79,7 @@ class vModule:
             return True
         return False
 
-    # @proprety
+    # @property
     # def maxV(self):
     #     return max(self.cellVoltage)
 
@@ -227,11 +230,54 @@ class vSystem:
                 # Trigger new system data event
                 self.newSysData.set()
 
+    def asDict(self, moduleID, formats=False) -> dict:
+        """
+        return the module reading values as a dict.  if formats=True, returns a tuple of the 
+        values and a dictionary of the print formats for convenience.
+        If formats = False, returns only the reading values dictionary.
+        """
+        mod = [m for m in self.modules if m.moduleVoltage > 1][0]  # find the matching moduleID
+        keynames = ['moduleID','soc','moduleVoltage','moduleUCVoltage','sumCellVoltage','moduleTemp','current']        
+        datafields = [mod.moduleID,mod.soc,mod.moduleVoltage,mod.moduleUCVoltage,sum(mod.cellVoltage),mod.moduleTemp,mod.current]
+        print_fmts = ['2.0f','3.2f','2.2f','2.2f','2.2f','2.1f','2.2f']
+        out_d = { k:v for (k,v) in zip(keynames, datafields) }
+        out_fmts = { k:v for (k,v) in zip(keynames, print_fmts) }
+        for cell in range(0,4):
+            out_fmts[f'cellVoltage_{cell}'] = '1.2f'
+            out_fmts[f'cellTemp_{cell}'] = '2.1f'
+            out_fmts[f'Balancing_{cell}'] = '1.0f'
+            out_d[f'cellVoltage_{cell}'] = mod.cellVoltage[cell]
+            out_d[f'cellTemp_{cell}'] = mod.cellTemp[cell]
+            if mod.cellBalStatus[cell]:   
+                out_d[f'Balancing_{cell}'] = 1
+            else:
+                out_d[f'Balancing_{cell}'] = 0
+        
+        assert len(out_fmts) == len(out_d), f"values dict and print formats dict must be same length!"
+        if formats:
+            return(out_d, out_fmts)
+        else:
+            return(out_d)
+
+
+
     def printStats(self, format='text'):
 
         m = 0
         self.dataLock.acquire()
+
         if format == 'csv':
+            for mod in [m for m in self.modules if m.moduleVoltage > 1]:
+                outstr = []
+                (val_strings, fmt_strings) = self.asDict(mod.moduleID, formats=True)
+                for key in val_strings:
+                    outstr.append(f'{val_strings[key]:{fmt_strings[key]}}')
+                # outstr = ','.join( [f'{v:{f}}' for v,f in zip(val_strings, fmt_strings)]  )
+                print(outstr)
+            pass    
+
+        
+        if format == 'csv2':
             self.moduleReadings = {}
             for mod in self.modules:
                 m += 1
@@ -255,13 +301,10 @@ class vSystem:
                     self.moduleReadings[mod.moduleID]['time'] = time.time()
                     pass
 
-
-
-        else: 
+        if format == 'text': 
             for module in self.modules:
                 m += 1
                 print('Module:%3d    %7.3fv - %7.3fv (%7.3fv)  %5.2fc  current: %5.2fa  SOC: %4.1f%%' % (module.moduleID,module.moduleVoltage,module.moduleUCVoltage,sum(module.cellVoltage),module.moduleTemp,module.current,module.soc))
-
                 for cell in range(0,4):
                     cellLine = '      Cell %3d  %5.3fv  %5.2fc' % (cell+1+((m-1)*4), module.cellVoltage[cell],module.cellTemp[cell])
                     if module.cellBalStatus[cell] == 1:
@@ -269,6 +312,8 @@ class vSystem:
                     print(cellLine)
     
         self.dataLock.release()
+
+
 
 
     def writeToInflux(self, fluxClient):
@@ -281,38 +326,41 @@ class vSystem:
             # "time": 1,
             }]
 
-        for (moduleID)  in self.moduleReadings:
+        for mod in [self.asDict(n) for n in range(len(self.modules))]:
             fields = {}
-            for (k,v) in self.moduleReadings[moduleID].items():
+            for (k,v) in mod.items():
                 if isinstance(v, Decimal):
                     v = float(v)
                 fields[k] = v
-            fields.pop('time')
+
             my_data = [{
                 "measurement": "Battery_Data",
-                "tags": {"moduleID": moduleID},
+                "tags": {"moduleTag": str(mod['moduleID'])},
                 "fields": fields,
 
                 # "time": 1,
                 }]
 
+        # fluxClient.write_data(my_data)
 
 
-        fluxClient.write_data(my_data)
 
 
-            
-        # write it
-
-
-        pass
-
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--time_interval", type=int, default=10, help="seconds between subsequent runs - default 30 sec")
+    parser.add_argument("-n", "--num_runs", type=int, default=4, help="number of runs to execute")
+    if len(sys.argv)==1:
+        parser.print_help(sys.stderr)
+        # sys.exit(1)
+    return parser.parse_args()
 
 
 
 
 
 if __name__ == '__main__':
+    args = parseArgs()
     sys = vSystem(1, 2, '/dev/ttyUSB0')
     load_dotenv()
     token = os.getenv('IFDB_TOKEN')
@@ -320,14 +368,18 @@ if __name__ == '__main__':
     bucket = os.getenv('IFDB_BUCKET')
     fluxClient = batt_influx.InfluxClient(token, org, bucket)
 
+
+    run_num = 0
     while True:
-        # print('hi.')
+        run_num += 1
         sys.newSysData.wait()
         sys.newSysData.clear()
-        sys.printStats(format='text')
         sys.printStats(format='csv')
         sys.writeToInflux(fluxClient)
-        time.sleep(30)
+        if run_num < args.num_runs:
+            time.sleep(args.time_interval)
+        else:
+            break
 
 
-
+    
